b75657a088f4bff7135f9b56ab7f1bc8af40ce60
NicoMontoya <nmontoy03@gmail.com>
2019-06-18 12:33:16 -0700

    connected to twitter and basilica API, added template and adjusted models to work with API

--- tweettweet/app.py
+++ tweettweet/app.py
@@ -1,14 +1,24 @@
-from flask import Flask
-from .models import DB 
+from decouple import config
+from flask import Flask, render_template, request 
+from .models import DB, Tweeter
 
 def create_app():
     """Create and configure an instance of the flask application"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route("/")
     def root():
-        return "Hello TweetTweet!"
+        tweeters = Tweeter.query.all()
+        return render_template('layout.html', title='Home', tweeters=tweeters)
+
+    @app.route("/reset")
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('layout.html', title='DB Reset!', tweeters=[])
 
     return app
--- tweettweet/models.py
+++ tweettweet/models.py
@@ -5,17 +5,19 @@ DB = SQLAlchemy()
 
 class Tweeter(DB.Model):
     """Twitter users that we pull and analyze tweets for"""
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     handle = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return "<Tweeter {}>".format(self.handle)
 
 class Tweet(DB.Model):
     """Tweets tweeted from the tweeters"""
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
-    tweeter_id = DB.Column(DB.Integer, DB.ForeignKey('tweeter.id'), nullable=False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    tweeter_id = DB.Column(DB.BigInteger, DB.ForeignKey('tweeter.id'), nullable=False)
     tweeter = DB.relationship('Tweeter', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
